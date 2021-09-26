851c5d5833c900c41662f8d9346b04f4bfd7a5d9
DESKTOP-RHPDI5I\Dell <pattaraporn.chaw@ku.th>
2021-09-26 17:23:20 +0700

    การย้าน กับ employee

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,26 @@
 <template>
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png"/> -->
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/>
     <faculty/>
-    <test/>
+    <test/> -->
+    <!-- <Employee/> -->
+    <penpraa/>
   </div>
 </template>
 
 <script>
 import Vue from "vue";
-import HelloWorld from './components/HelloWorld.vue';
-import faculty from './components/faculty.vue';
-import test from './components/test.vue';
+// import HelloWorld from './components/HelloWorld.vue';
+// import faculty from './components/faculty.vue';
+// import test from './components/test.vue';
+// import Employee from './components/Employee.vue';
 //Bootstarp
 import {BootstrapVue , IconsPlugin} from 'bootstrap-vue'
 import "bootstrap/dist/css/bootstrap.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
+import Penpraa from './components/penpraa.vue';
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
@@ -23,11 +28,14 @@ Vue.use(IconsPlugin)
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-    faculty,
-    test
+    // HelloWorld,
+    // faculty,
+    // test,
+    // Employee
+     Penpraa
     }
   };
+   
 
 </script>
 
---
+++ src/components/penpraa.vue
@@ -0,0 +1,95 @@
+<template>
+  <div id="pen" class="par">
+      <img v-bind:src = url
+      width = "20%" height="20%"/><br><br>
+    <h4>ห่มจีวรคือเป็นพระห่ม ผ้าด้วยนะคือเป็นห่วง</h4>
+
+    <div class="input-group mb-3">
+      <span class="input-group-text" id="inputGroup-sizing-sm">Id</span>
+      <input
+        type="text"
+        class="form-control"
+        aria-label="Sizing example input"
+        v-model="info.id"
+      />
+    </div>
+
+    <div class="input-group mb-3">
+      <span class="input-group-text" id="inputGroup-sizing-sm">First Name</span>
+      <input
+        type="text"
+        class="form-control"
+        aria-label="Sizing example input"
+        v-model="info.firstName"
+      />
+    </div>
+
+    <div class="input-group mb-3">
+      <span class="input-group-text" id="inputGroup-sizing-sm">Last Name</span>
+      <input
+        type="text"
+        class="form-control"
+        aria-label="Sizing example input"
+        v-model="info.lastName"
+      />
+    </div>
+
+    <div class="input-group mb-3">
+      <span class="input-group-text" id="inputGroup-sizing-sm">Nick Name</span>
+      <input
+        type="text"
+        class="form-control"
+        aria-label="Sizing example input"
+        v-model="info.NickName"
+      />
+    </div>
+
+    <button type="button" class="btn btn-warning" @click="save">Save</button>
+
+    <div :hidden="!show">
+     <b-table striped hover :items="myFriends"></b-table>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "employee",
+  data() {
+    return {
+     url : 'https://f.ptcdn.info/777/057/000/p9ae2davnpo1z5gWg9S-o.jpg',
+      info: {
+        id:null,
+        firstName: null,
+        lastName: null,
+        NickName: null,
+      },
+      show: false,
+
+      myFriends: [],
+    };
+  },
+  methods: {
+    save() {
+      this.myFriends.push(this.info);
+      this.show = true;
+      this.info = {
+        id:null,
+        firstName: null,
+        lastName: null,
+        NickName: null,
+      };
+
+    },
+
+  },
+};
+</script>
+
+<style>
+.par {
+  background :rgb(255, 197, 120);
+  color: orangered;
+}
+</style>
